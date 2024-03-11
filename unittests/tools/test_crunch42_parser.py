7ee6644ee5f785fe07471ec357a60615554de413
Felix Hernandez <ffhg_920522@hotmail.com>
2024-03-11 12:54:59 -0600

    added empty tests

--- unittests/tools/test_crunch42_parser.py
+++ unittests/tools/test_crunch42_parser.py
@@ -5,6 +5,13 @@ from dojo.tools.crunch42.parser import Crunch42Parser
 
 class TestCrunch42Parser(DojoTestCase):
 
+    def test_crunch42parser_empty(self):
+        testfile = open("unittests/scans/crunch42/crunch42_empty.json")
+        parser = Crunch42Parser()
+        findings = parser.get_findings(testfile, Test())
+        testfile.close()
+        self.assertEqual(0, len(findings))
+
     def test_crunch42parser_single_has_many_findings(self):
         testfile = open("unittests/scans/crunch42/crunch42_many_findings.json")
         parser = Crunch42Parser()
